8e4908ffed0c4c375e99d9b4545095e8964455b3
Nunes Rgns <infobasic20@gmail.com>
2026-05-23 14:59:26 +0000

    ajustes em variaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,7 +4,7 @@
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 // <>--> Código corrigido, formato das variáveis na inicialização e na saída de dados
-// 23 de maio as 10:18
+// 23 de maio as 11:59
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
   // Carta 1
