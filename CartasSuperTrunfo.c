f58a2b615d2b92077f58dce17e120508d3943015
Nunes Rgns <infobasic20@gmail.com>
2026-05-23 14:58:26 +0000

    incluindo variaveis de resultado para o nivel mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -20,6 +20,16 @@ int main() {
     float densidade = 0.0f;
     float PIBPerCapita = 0.0f;
 
+    // Implementação do nível mestre
+    float SuperPoder = 0.0f;
+    int ResultPopulacao = 0;
+    int ResultArea = 0;
+    int ResultPIB = 0;
+    int ResultnumPontoTuristico = 0;
+    int ResultDensidade = 0;
+    int ResultPIBPerCapita = 0;
+
+    
   // Carta 2
   // Esta não é uma prática correta. o código foi duplicado porque a instrução era não usar laço na lógica.
   char estado2;
@@ -32,6 +42,15 @@ int main() {
 // acrescentado no nível intermediário
     float densidade2 = 0.0f;
     float PIBPerCapita2 = 0.0f;
+
+ // Implementação do nível mestre
+    float SuperPoder2 = 0.0f;
+    int ResultPopulacao2 = 0;
+    int ResultArea2 = 0;
+    int ResultPIB2 = 0;
+    int ResultnumPontoTuristico2 = 0;
+    int ResultDensidade2 = 0;
+    int ResultPIBPerCapita2 = 0;
 
 
   // Área para entrada de dados
@@ -87,9 +106,16 @@ printf("  ******  Entre com os dados da carta 2 *****  \n");
     PIBPerCapita = (PIB / populacao);    // pib per capita da cidade 1
     PIBPerCapita2 = (PIB2 / populacao2); // pib per capita da cidade 2
 
+  // Compara valores das cartas
+    ResultPopulacao = populacao > populacao2;
+    ResultArea = area > area2;
+    ResultPIB = PIB > PIB2;
+    ResultnumPontoTuristico = numPontoTuristico > numPontoTuristico2;
+    ResultDensidade = densidade > densidade2;
+    ResultPIBPerCapita = PIBPerCapita > PIBPerCapita2;
 
   // Área para exibição dos dados da cidade 1
-    printf("  ******  exibição dos dados digitados por carta *****  \n");
+    printf(" ******  exibição dos dados digitados por carta *****  \n");
     printf("Estado: %c\n", estado);
     printf("Código da Carta: %s \n", codCarta);
     printf("Cidade: %s \n", nomeCidade);
@@ -114,8 +140,16 @@ printf("  ******  Entre com os dados da carta 2 *****  \n");
     printf("Densidade populacional da Cidade: %.2f .\n", densidade2 );
     printf("PIB per capita: %.2f .\n", PIBPerCapita2);
  
-
     printf(" ---------------------------------------  \n");
+    printf(" -----------  Comparando as cartas -------- \n");
+    printf("A população da cidade %s é maior que da cidade %s ? %d. \n", nomeCidade, nomeCidade2, ResultPopulacao);
+    printf("A area da cidade %s é maior que a da cidade %s ? %d. \n", nomeCidade, nomeCidade2, ResultArea);
+    printf("O PIB da cidade %s é maior que o da cidade %s ? %d. \n", nomeCidade, nomeCidade2, ResultPIB);
+    printf("Há mais pontos turisticos na cidade %s do que na cidade %s ? %d. \n", nomeCidade, nomeCidade2, ResultnumPontoTuristico);
+    printf("A densidade populacional da cidade %s é maior que o da cidade %s ? %d. \n", nomeCidade, nomeCidade2, ResultDensidade);
+    printf("PIB per capta da cidade %s é maior que o da cidade %s ? %d. \n", nomeCidade, nomeCidade2, ResultPIBPerCapita);
+
+
     
 return 0;
 } 
